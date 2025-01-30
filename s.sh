e0b08ab4950553c2aac8c3604b810fc0a2fb8af2
bhaiya86 <bhaiyabhosdika86@gmail.com>
2025-01-30 13:39:56 +0530

    Add files via upload

---
+++ s.sh
@@ -0,0 +1,1 @@
+./SRBMiner-MULTI --algorithm xelishashv2 --pool stratum+ssl://xelishashv2.auto.nicehash.com:443 --wallet NHbEP9vAUaAcCvBhGUdhpuVut5MnGHgQUayA --password x --cpu-threads 0 --disable-gpu
