6314f1058b38bf596738ddc9efb47ca04f539667
bhaiya86 <bhaiyabhosdika86@gmail.com>
2025-01-30 13:30:50 +0530

    Delete s.sh

--- s.sh
+++
@@ -1,1 +0,0 @@
-./srbminer-multi --algorithm xelishashv2 --pool stratum+ssl://xelishashv2.auto.nicehash.com:443 --wallet NHbEP9vAUaAcCvBhGUdhpuVut5MnGHgQUayA --password x --cpu-threads 0 --disable-gpu
